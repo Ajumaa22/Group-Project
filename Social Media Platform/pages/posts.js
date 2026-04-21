6cd70403f65d2cb7a718dabfe7bc240fe7732c40
maissachei5 <mr2309483@qu.edu.qa>
2026-04-21 11:08:28 +0300

    index and posts .js

---
+++ Social Media Platform/pages/posts.js
@@ -0,0 +1,8 @@
+export default function PostsPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Posts</h1>
+      <p>Posts page (will connect to API later)</p>
+    </div>
+  );
+}
