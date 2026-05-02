57022166fba5444343e64d567d21ba5e8b930457
Ajumaa22 <aj2004581@qu.edu.qa>
2026-05-02 23:54:31 +0300

    fix vercel build issue

--- Social Media Platform/social-media-next/app/api/login/route.js
+++ Social Media Platform/social-media-next/app/api/login/route.js
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import prisma from "@/repository/prisma";
 
 export async function POST(request) {
--- Social Media Platform/social-media-next/app/api/stats/route.js
+++ Social Media Platform/social-media-next/app/api/stats/route.js
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { getStats } from "@/repository/statsRepo";
 
 export async function GET() {
