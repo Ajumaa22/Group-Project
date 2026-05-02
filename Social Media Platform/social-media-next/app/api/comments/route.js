192ef39c601ba5a32d0dc5211606633adde53a28
Ajumaa22 <aj2004581@qu.edu.qa>
2026-05-02 23:59:03 +0300

    fix comments api dynamic import

--- Social Media Platform/social-media-next/app/api/comments/route.js
+++ Social Media Platform/social-media-next/app/api/comments/route.js
@@ -1,17 +1,20 @@
 export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server";
-import { addComment, deleteComment } from "@/repository/postRepo";
 
 export async function POST(req) {
+  const { addComment } = await import("@/repository/postRepo");
+
   const { userId, postId, content } = await req.json();
   const comment = await addComment(userId, postId, content);
+
   return NextResponse.json(comment, { status: 201 });
 }
 
 export async function DELETE(req) {
-  const { commentId } = await req.json();
+  const { deleteComment } = await import("@/repository/postRepo");
 
+  const { commentId } = await req.json();
   await deleteComment(commentId);
 
   return NextResponse.json({ message: "Comment deleted" });
